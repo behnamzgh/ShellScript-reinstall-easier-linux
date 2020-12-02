f58e7fe0f284a0184fe54c4d4cb3fd77059c2d04
behnam <codepoete@gmail.com>
2020-12-02 05:36:19 +0330

    Add files via upload

---
+++ ScriptMakeReinstallUbuntuEasier.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ~
+sudo apt update -qq
+sudo apt install -yy htop gdebi
+
+sudo apt install -yy openssh-server virtualbox
+sudo adduser ${USERNAME} vboxusers
+
+cd /home/codepoete/Downloads
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo gdebi -n google-chrome-stable_current_amd64.deb
+sudo rm -f google-chrome-stable_current_amd64.deb
+cd ~
+
+echo "All done! please reboot the system."
