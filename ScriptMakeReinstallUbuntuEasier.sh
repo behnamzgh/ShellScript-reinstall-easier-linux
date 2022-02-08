d61c423e28d8c23f181204e179c512fa36f6d09d
behnamzgh <behnamzgh@gmail.com>
2022-02-08 15:31:15 +0330

    -

--- ScriptMakeReinstallUbuntuEasier.sh
+++ ScriptMakeReinstallUbuntuEasier.sh
@@ -2,10 +2,10 @@
 
 cd ~
 sudo apt update -qq
-sudo apt install -yy htop gdebi terminator vim curl screenfetch aria2 apache2 speedtest bat unrar php phpmyadmin mariadb-server build-essential gcc loc openssh-server
+sudo apt install -yy htop gdebi vim curl screenfetch aria2 apache2 bat unrar php phpmyadmin mariadb-server build-essential gcc loc openssh-server
 
-sudo apt install -yy virtualbox
-sudo adduser ${USER} vboxusers
+# sudo apt install -yy virtualbox
+# sudo adduser ${USER} vboxusers
 
 cd /home/codepoete/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
